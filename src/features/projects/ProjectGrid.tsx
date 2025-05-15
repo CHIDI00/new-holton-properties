858f67194eef930cf1ea7797ffc4618dbcc6902b
DevCHIDI <chidimathayas@gmail.com>
2025-05-15 18:49:06 +0100

    fix: adjust layout and styling in ProjectGrid component for improved visual consistency

--- src/features/projects/ProjectGrid.tsx
+++ src/features/projects/ProjectGrid.tsx
@@ -6,14 +6,14 @@ const ProjectGrid: React.FC = () => {
 	return (
 		<div className="w-full py-12">
 			<div className="container md:px-[11rem] px-[2rem] mx-auto grid md:grid-cols-3 grid-cols-1 gap-14">
-				<div className="group relative w-full md:h-[47rem] h-[50rem] bg-red-200 rounded-[3rem] overflow-hidden">
+				<div className="group relative w-full md:h-[50rem] h-[50rem] bg-red-200 rounded-[3rem] overflow-hidden">
 					<div
 						className="absolute top-0 left-0 w-full h-full bg-cover bg-center bg-no-repeat rounded-[3rem] group-hover:scale-105 transition-transform duration-500 ease-in-out"
 						style={{ backgroundImage: `url(${image})` }}
 					></div>
-					<div className="absolute flex flex-col justify-between items-center z-[4] bg-gradient-to-b from-[#00000000] to-[#0000009e] w-full h-full p-6">
+					<div className="absolute flex flex-col justify-between items-center z-[4] bg-gradient-to-b from-[#00000000] to-[#000000d2] w-full h-full md:p-12 md:px-16 p-16">
 						<div className="w-full flex justify-start items-start">
-							<span className="bg-[#2A286B] rounded-full md:text-[1.7rem] text-2xl text-white px-7 py-4 font-bold">
+							<span className="bg-[#2A286B] rounded-full md:text-[1.2rem] text-2xl text-white md:px-5 md:py-2 px-7 py-4 font-bold uppercase">
 								Completed
 							</span>
 						</div>
@@ -25,7 +25,7 @@ const ProjectGrid: React.FC = () => {
 								</span>
 								Lekki, Phase 2
 							</p>
-							<h1 className="md:text-[3rem] text-[2.5rem] text-white pt-6 font-semibold">
+							<h1 className="md:text-7xl text-7xl text-white pt-6 font-bold">
 								Apartment building
 							</h1>
 						</div>
